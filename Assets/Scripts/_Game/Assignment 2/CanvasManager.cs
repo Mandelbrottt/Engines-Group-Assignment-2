b22c806f306eb7a46fb6e62170cfa75c789e5841
Nathan Tuck <nathan.tuck1@ontariotechu.net>
2020-11-07 22:21:07 -0500

    Proper icon switching
    Still based on a key instead of triggers, but we're out here

--- Assets/Scripts/_Game/Assignment 2/CanvasManager.cs
+++ Assets/Scripts/_Game/Assignment 2/CanvasManager.cs
@@ -8,32 +8,44 @@ using TMPro;
 public class CanvasManager : MonoBehaviour
 {
     public TMP_Text Objective1;
-    public Image JumpIcon;
-    public Image WallrunIcon;
-    public Image SwapGunIcon;
-    public Image ShootEnemyIcon;
-    public Image ShootWeakspotIcon;
-    public Image IncompleteCheckbox;
-    public Image CompleteCheckbox;
+    public Sprite JumpIcon;
+    public Sprite WallrunIcon;
+    public Sprite SwapGunIcon;
+    public Sprite ShootEnemyIcon;
+    public Sprite ShootWeakspotIcon;
+    public Sprite IncompleteCheckbox;
+    public Sprite CompleteCheckbox;
+    public Sprite NextRoomIcon;
+    public Image currentIcon;
+    public Image currentCheckbox;
 
     bool isCoroutineRunning = false;
      
     int objectiveIndex = 0;
 
     string[] objectiveStrings = {"Press [Space] to Jump", "Continue to next room", "Hold [W] and Jump next to a wall to Wallrun",
-                                "Continue to next room", "Wallrun the corner to reach the exit", "Press 1, 2, 3 or 4 to swap weapons",
+                                "Continue to next room", "Wallrun the curved wall to reach the exit", "Press 1, 2, 3 or 4 to swap weapons",
                                 "Shoot the frog", "Shoot the frog in the head", "Continue to next room",
                                 "Play around and escape when satisfied"};
 
+    Sprite[] objectiveImages;
+
+    private void Awake()
+    {
+        objectiveImages = new Sprite[] {JumpIcon, NextRoomIcon, WallrunIcon, NextRoomIcon, NextRoomIcon, SwapGunIcon, ShootEnemyIcon,
+                                       ShootWeakspotIcon, NextRoomIcon, NextRoomIcon};
+    }
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         Objective1.text = objectiveStrings[objectiveIndex];
     }
 
+    
+
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if(Input.GetKeyDown(KeyCode.F) && !isCoroutineRunning)
         {
@@ -50,15 +62,19 @@ public class CanvasManager : MonoBehaviour
         {
             alpha.a = i / 10f;
             Objective1.color = alpha;
+            currentIcon.color = alpha;
             yield return new WaitForSeconds(0.1f);
         }
 
-        Objective1.text = objectiveStrings[++objectiveIndex];
+        objectiveIndex++;
+        Objective1.text = objectiveStrings[objectiveIndex];
+        currentIcon.sprite = objectiveImages[objectiveIndex];
 
-        for (int i = 0; i <= 9; i++)
+        for (int i = 1; i <= 10; i++)
         {
             alpha.a = i / 10f;
             Objective1.color = alpha;
+            currentIcon.color = alpha;
             yield return new WaitForSeconds(0.1f);
         }
 
