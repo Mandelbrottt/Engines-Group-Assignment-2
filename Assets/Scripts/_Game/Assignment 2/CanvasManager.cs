949246b05ab6b1c3a3f979a35967fcae99688d3d
Nathan Tuck <nathan.tuck1@ontariotechu.net>
2020-11-07 20:23:11 -0500

    Added mostly empty canvas manager

---
+++ Assets/Scripts/_Game/Assignment 2/CanvasManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+    public Text Objective1;
+    public Text Objective2;
+    public Sprite JumpIcon;
+    public Sprite WallrunIcon;
+    public Sprite SwapGunIcon;
+    public Sprite ShootEnemyIcon;
+    public Sprite ShootWeakspotIcon;
+    public Sprite IncompleteCheckbox;
+    public Sprite CompleteCheckbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Cycle which icons are being displayed using room trigger volumes
+    //and update the objective text with the current objective
+}
